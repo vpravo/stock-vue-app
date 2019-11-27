bdada430ed92885a5c76da6d9fb1e2475675eda3
vpravo <yoursluzhba@gmail.com>
2019-11-27 09:38:01 +0300

    Stock компонент

--- src/components/stocks/stocks.vue
+++ src/components/stocks/stocks.vue
@@ -1,10 +1,26 @@
 <template>
-  <h1>Stocks component</h1>
+  <div>
+    <app-stock v-for="(stock, i) in stocks" :key="i" :stock="stock"
+    ></app-stock>
+  </div>
 </template>
 
 <script>
+import Stock from "./stock.vue"
 export default {
-
+  data(){
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 220},
+        {id: 3, name: 'Apple', price: 270},
+        {id: 4, name: 'Twitter', price: 300},
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
 }
 </script>
 
