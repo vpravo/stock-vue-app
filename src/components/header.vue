994f257c70b7b220caff8afb2eb8887fbe74eaf0
vpravo <yoursluzhba@gmail.com>
2019-11-27 22:27:31 +0300

    Анимация

--- src/components/header.vue
+++ src/components/header.vue
@@ -12,7 +12,10 @@
                 <strong class="navbar-text navbar-right">Funds: {{funds | currency}}</strong>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#" @click="endDay">End Day</a></li>
-                    <li class="dropdown">
+                    <li class="dropdown"
+                        :class="{open:  isDropDownOpen}"
+                        @click="isDropDownOpen = !isDropDownOpen"
+                        >
                         <a
                             href="#"
                             class="dropdown-toggle"
@@ -35,6 +38,11 @@
 <script>
 import {mapActions} from "vuex"
 export default {
+    data(){
+        return {
+            isDropDownOpen: false
+        }
+    },
     computed: {
         funds(){
             return this.$store.getters.funds
